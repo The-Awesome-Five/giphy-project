1cc462a7e2044ccbe7260cc42ab04335fd431e09
Svetoslav <sberberski@gmail.com>
2024-07-10 19:07:06 +0300

    Category-view impl

---
+++ src/views/category-view.js
@@ -0,0 +1,15 @@
+export const toGifCategorieView = async (data) => {
+    const gifUrls = await Promise.all(data.map(el => toGif(el)));
+    return `
+      <div id="gifs">
+        <h1>${data[0].name}:</h1>
+        <div class="container">
+          ${gifUrls.map(url => `<img id="gif" src="${url}" alt="Gif">`).join('\n')}
+        </div>
+      </div>
+    `;
+  };
+  
+  export const toGif = async (data) => {
+    return data.image.original.url;
+  };
--- src/views/view-about.js
+++ src/views/view-about.js
@@ -1,6 +1,6 @@
 export const toAboutView = () => `
 <div id="about">
-  <div class="content">
+  <div class="container">
    <h1 class="heading-about">About the app</h1>
    <p> Placeholder name is a simple app that lets up search, upload and mark gifs a favorite </p>
    <p> It's completely implemented with vanilla JavaScript (Native DOM API) and vanilla CSS and API. </p>
