9da83f29314762f06724f98f3ecd3ec888b66823
Svetoslav <sberberski@gmail.com>
2024-07-11 12:04:31 +0300

    about-view  css updated

--- src/views/view-about.js
+++ src/views/view-about.js
@@ -1,6 +1,5 @@
 export const toAboutView = () => `
 <div id="about-view">
-  <div class="container">
    <h1 class="heading-about">About the app</h1>
    <p> Placeholder name is a simple app that lets up search, upload and mark gifs a favorite </p>
    <p> It's completely implemented with vanilla JavaScript (Native DOM API) and vanilla CSS and API. </p>
