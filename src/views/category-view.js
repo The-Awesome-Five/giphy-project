60406bfebbbed9040b0bdfb38a4cdad82e456437
Svetoslav <sberberski@gmail.com>
2024-07-11 11:26:04 +0300

    Updated Cat View impl

--- src/views/category-view.js
+++ src/views/category-view.js
@@ -1,16 +1,28 @@
 export const toGifCategorieView = async (data, name) => {
+  let counter = 0;
+  let cols = [[], [], []];
+  const gifUrls = await Promise.all(data.map(el => toGif(el)));
 
-    const gifUrls = await Promise.all(data.map(el => toGif(el)));
-    return `
-      <div id="gifs">
-        <h1>${name}:</h1>
-        <div class="container">
-          ${gifUrls.map(url => `<img id="gif" src="${url}" alt="Gif">`).join('\n')}
-        </div>
+  gifUrls.forEach((url, index) => {
+    const gifElement = `<div class="gif"><img src="${url}" alt="Gif"></div>`;
+    cols[counter].push(gifElement);
+    counter = (counter + 1) % 3;
+  });
+
+  return `
+    <h1>${name}:</h1>
+    <div id="container">
+      <div class="gif-gallery">
+        ${cols.map((col, index) => `
+          <div id="col-${index}">
+            ${col.join('\n')}
+          </div>
+        `).join('\n')}
       </div>
-    `;
-  };
-  
-  export const toGif =  (data) => {
-    return  data.images.original.url;
-  };
+    </div>
+  `;
+};
+
+export const toGif = (data) => {
+  return data.images.original.url;
+};
