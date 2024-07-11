d7dd029a27232a4e3d6f80259c9c63b55d767cc0
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-07-11 12:11:08 +0300

    Update giphy-events.js

--- src/events/giphy-events.js
+++ src/events/giphy-events.js
@@ -2,7 +2,6 @@ import {uploadGif} from "../requests/giphy-service.js";
 
 export const handleUpload = async (file) => {
 
-    console.log(file)
     const body = new FormData();
 
     body.append('file', file);
