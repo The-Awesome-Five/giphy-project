a3f41f5990b7c3c14fa4afb2c23a1a80f6771dd8
Svetoslav <sberberski@gmail.com>
2024-07-12 18:27:13 +0300

    Bug Fix Views

--- src/events/detailed-even.js
+++ src/events/detailed-even.js
@@ -5,6 +5,11 @@ import { toDetailedView } from "../views/detailed-view.js";
 export const renderDetailedView = async (id) => {
     // gifs
     const gifInfo = await getGIfById(id)
-    const views= await getViewCount(id);
+    const views = await getViewCount(id) || 0;
+    if(views===0) {
+        document.querySelector(CONTAINER_SELECTOR).innerHTML = await toDetailedView(gifInfo.data, views);
+    }
+    else{
     document.querySelector(CONTAINER_SELECTOR).innerHTML = await toDetailedView(gifInfo.data, views.viewCount);
+    }
 };
