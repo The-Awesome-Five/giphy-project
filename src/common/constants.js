9e92786707157cfb8fb800c83b061f3e042428dd
Svetoslav <sberberski@gmail.com>
2024-07-13 00:46:33 +0300

    2keys

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,5 @@
 export const API_URL = 'https://api.giphy.com/v1/gifs'
+// export const API_KEY = 'IzXGz27fvHjfGwTEuGtIzVZKw34LTNsy'
 export const API_KEY = 'HkRmTzX5UR0D6Uxa8zZWYum0KNFtnULt'
 export const ABOUT= 'about';
 export const GIFS= 'gif_home';
