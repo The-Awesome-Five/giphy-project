ce191458bacec0d32ddffd42528ca40a4346e250
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-07-12 03:22:30 +0300

    set-logo-GifHub

--- src/views/view-about.js
+++ src/views/view-about.js
@@ -1,7 +1,7 @@
 export const toAboutView = () => `
 <div id="about-view">
   <div id="container">
-      <h1 class="heading-about">About the app</h1>
+      <img src="img/gif-huv-logo.png" height="130">
       <p> Placeholder name is a simple app that lets up search, upload and mark gifs a favorite </p>
       <p> It's completely implemented with vanilla JavaScript (Native DOM API) and vanilla CSS and API. </p>
       <h2>Author Group: Group 5</h2>
