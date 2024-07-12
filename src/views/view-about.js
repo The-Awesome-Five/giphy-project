ca5fea895c5780781b3338b93b696a7753aac35f
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-07-12 12:16:44 +0300

    about-view-main-update

--- src/views/view-about.js
+++ src/views/view-about.js
@@ -1,10 +1,23 @@
 export const toAboutView = () => `
 <div id="about-view">
   <div id="container">
-      <img src="img/gif-huv-logo.png" height="130">
-      <p> Placeholder name is a simple app that lets up search, upload and mark gifs a favorite </p>
-      <p> It's completely implemented with vanilla JavaScript (Native DOM API) and vanilla CSS and API. </p>
-      <h2>Author Group: Group 5</h2>
+      <h1>welcome to github</h1>
+      <p>
+        At gifHub, we believe in the power of visual storytelling. 
+        Our platform is dedicated to providing a seamless and enjoyable experience for creating, 
+        sharing, and discovering animated GIFs that capture the essence of your moments, ideas, and emotions.
+      </p>
+      <p> </p>
+      <p> </p>
+      <h1>DEvelopers</h1>
+      <div id="container-dev">
+            <img src="img/avatars/svet.png" height="130">
+            <img src="img/avatars/vlad.png" height="130">
+            <img src="img/avatars/doni.png" height="130">
+            <p>Vlad</p>
+            <p>Svet</p>
+            <p>Doni</p>
+        </div>
       </div>
   </div>
 </div>
