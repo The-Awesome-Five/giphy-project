9a9f8b071617f0baf44debf2c2875fc43c6d47f4
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-07-14 17:24:22 +0300

    upload-view+update-cat-btns

--- src/views/upload-view.js
+++ src/views/upload-view.js
@@ -1,15 +1,19 @@
 export const toUploadView = () =>`
   <div id="upload">
-    <h1>Upload Gif:</h1>
-        <div id="container">
+    <h1>Upload Gif</h1>
+        <div id="container-upload">
+        <img id="preview-image" src="" alt="" />
             <form id="upload-form" class="upload">
+                <label for="gif-upload" class="file-upload-label">Choose a nude gif</label>
                 <input type="file" id="gif-upload" accept="image/gif" required />
+
                 <button type="submit">Upload</button>
+              
             </form>
+            
         </div>    
     <div id="upload-status">
-    <img id="preview-image" src="" alt="preview-image" />
+    
 </div>
   </div>
 `;
-
